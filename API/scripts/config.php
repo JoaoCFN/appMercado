0f191ee17d109a6e0f4f4574a99fa09a524504bc
bl4nc <40602200+bl4nc@users.noreply.github.com>
2020-12-15 13:41:19 -0300

    .

--- API/scripts/config.php
+++ API/scripts/config.php
@@ -1,4 +1,15 @@
 <?php 
 $local_db = '../../dbapp.db';
+$con = new SQLite3($local_db);
+$query = ("INSERT into compra (nome_lista,valor_total,status) values ('Lista de Testes','450','Pendente')");
+$con ->exec($query);
+
+if ($query){
+    echo ('gg');
+}else{
+    echo ('wp');
+}
+
+echo ($local_db);
 
 ?>
